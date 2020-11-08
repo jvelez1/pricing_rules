7ba3129abd7b3f267a067ee81b83c5ba44480285
josue <josuevelezdr@gmail.com>
2020-11-08 14:25:37 +0100

    Initial commit

---
+++ lib/pricing_rules.rb
@@ -0,0 +1,6 @@
+require "pricing_rules/version"
+
+module PricingRules
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/pricing_rules/version.rb
@@ -0,0 +1,3 @@
+module PricingRules
+  VERSION = "0.1.0"
+end
