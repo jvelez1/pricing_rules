d5d844c1669e18d3627b2d9399175bca80473c93
josue <josuevelezdr@gmail.com>
2020-11-09 13:36:27 +0100

    Formatting checkout spec

--- spec/pricing_rules/checkout_spec.rb
+++ spec/pricing_rules/checkout_spec.rb
@@ -2,52 +2,52 @@ module PricingRules
   RSpec.describe Checkout do
     PRODUCTS = [
       {
-        "name": "Green tea",
-        "code": "GR1",
-        "price": 3.11
+        name: "Green tea",
+        code: "GR1",
+        price: 3.11
       },
       {
-        "name": "Strawberries",
-        "code": "SR1",
-        "price": 5
+        name: "Strawberries",
+        code: "SR1",
+        price: 5
       },
       {
-        "name": "Coffe",
-        "code": "CF1",
-        "price": 11.23
+        name: "Coffe",
+        code: "CF1",
+        price: 11.23
       }
     ]
 
     RULES = [
       {
-        "on": "GR1",
-        "rule":{
-          "class": "BuyAndGet",
-          "options":{
-              "buy":1,
-              "get":1
+        on: "GR1",
+        rule: {
+          class: "BuyAndGet",
+          options: {
+              buy: 1,
+              get: 1
             }
          }
       },
       {
-        "on": "SR1",
-        "rule":{
-          "class": "DiscountBulkOnPriceValue",
-          "options":{
-              "validation": "greater_than_or_equal_to",
-              "quantity":3,
-              "forced_price":4.5
+        on: "SR1",
+        rule: {
+          class: "DiscountBulkOnPriceValue",
+          options: {
+              validation: "greater_than_or_equal_to",
+              quantity: 3,
+              forced_price: 4.5
             }
          }
       },
       {
-        "on": "CF1",
-        "rule":{
-          "class": "DiscountBulkOnPricePercentage",
-          "options":{
-              "validation": "greater_than_or_equal_to",
-              "percentage":0.6667,
-              "quantity":3
+        on: "CF1",
+        rule: {
+          class: "DiscountBulkOnPricePercentage",
+          options: {
+              validation: "greater_than_or_equal_to",
+              percentage: 0.6667,
+              quantity: 3
             }
          }
       }
@@ -55,19 +55,19 @@ module PricingRules
 
     TEST_CASES = [
       {
-        basket: "GR1,SR1,GR1,GR1,CF1",
+        basket: 'GR1,SR1,GR1,GR1,CF1',
         total_price_expected: 22.45
       },
       {
-        basket: "GR1,GR1",
+        basket: 'GR1,GR1',
         total_price_expected: 3.11
       },
       {
-        basket: "SR1,SR1,GR1,SR1",
+        basket: 'SR1,SR1,GR1,SR1',
         total_price_expected: 16.61
       },
       {
-        basket: "GR1,CF1,SR1,CF1,CF1",
+        basket: 'GR1,CF1,SR1,CF1,CF1',
         total_price_expected: 30.57
       }
     ]
